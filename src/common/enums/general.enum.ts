7d46bccdc5f0358d9d41a999b6079d848e206abf
Kasra Karami <Kasra_K2K@yahoo.com>
2023-02-08 18:22:15 +0330

    add common

---
+++ src/common/enums/general.enum.ts
@@ -0,0 +1,30 @@
+export enum ServiceName {
+  DEFAULT,
+  USER,
+  ARTICLE,
+  SUPPORT,
+  ASSET,
+  GATEWAY
+}
+
+export enum RoleName {
+  USER,
+  ADMIN
+}
+
+export enum PermissionName {
+  CUSTOMER,
+  WRITER,
+  PARTNER,
+  ADMIN
+}
+
+export enum TokenType {
+  TOKEN,
+  REFRESF_TOKEN
+}
+
+export enum StrategyName {
+  ADMIN_USER = 'admin_user',
+  APP_USER = 'app_user'
+}
